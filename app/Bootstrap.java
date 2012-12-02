312733fdc1f49f535172122dabc61f3f5827c92b
kh156 <kuanghan.kevin@gmail.com>
2012-12-02 18:28:49 -0500

    added init data

---
+++ app/Bootstrap.java
@@ -0,0 +1,19 @@
+import play.*;
+import play.jobs.*;
+import play.test.*;
+ 
+import models.*;
+ 
+@OnApplicationStart
+public class Bootstrap extends Job {
+ 
+    public void doJob() {
+        // Check if the database is empty
+        System.out.println("I'm here!!!!");
+
+        if(User.count() == 0) {
+            Fixtures.loadModels("initial-data.yml");
+        }
+    }
+ 
+}
